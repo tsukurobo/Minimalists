d48ee9ce0f2755a7c2d9d4569dd02e9b23132ee5
Neko2234 <99799346+Neko2234@users.noreply.github.com>
2025-09-13 21:43:56 +0900

    chore: パラメータ微調整

--- include/config.hpp
+++ include/config.hpp
@@ -35,7 +35,7 @@ constexpr uint8_t ERROR_PIN = 19;
 namespace ShootingConfig {
 constexpr uint8_t SERVO_PIN = 12;
 constexpr float IDLE_ANGLE = 0.0f;         // 待機時の角度
-constexpr float CORRECTION_ANGLE = 30.0f;  // ワークの姿勢を整えるときの角度
+constexpr float CORRECTION_ANGLE = 90.0f;  // ワークの姿勢を整えるときの角度
 }  // namespace ShootingConfig
 
 // 軌道データ点の構造体（制御用の詳細軌道）
--- src/main.cpp
+++ src/main.cpp
@@ -928,9 +928,12 @@ int main(void) {
     while (1) {
         // シューティングエリアのサーボを3秒に1回動かす
         static absolute_time_t last_shoot_servo_time = {0};
-        if (absolute_time_diff_us(last_shoot_servo_time, get_absolute_time()) >= 2'000'000) {
+        if (absolute_time_diff_us(last_shoot_servo_time, get_absolute_time()) >= 3'000'000 &&
+            absolute_time_diff_us(last_shoot_servo_time, get_absolute_time()) < 6'000'000) {
+            g_debug_manager->debug("Set shooting servo angle to correction angle.");
             shooting_servo.set_angle(ShootingConfig::CORRECTION_ANGLE);
-        } else if (absolute_time_diff_us(last_shoot_servo_time, get_absolute_time()) >= 3'000'000) {
+        } else if (absolute_time_diff_us(last_shoot_servo_time, get_absolute_time()) >= 6'000'000) {
+            g_debug_manager->debug("Set shooting servo angle to idle angle.");
             shooting_servo.set_angle(ShootingConfig::IDLE_ANGLE);
             last_shoot_servo_time = get_absolute_time();
         }
